fb617f8a1af0b96adf1c381a65af3cfce155eb28
Lisa Rolczynski <lisa.sano@gmail.com>
2016-04-07 14:57:00 -0700

    added a new method to determine whether the word or list of words given contains non-letter characters (besides a comma for the list).  Will return a message saying "not a word" when appropriate.

--- lib/score.rb
+++ lib/score.rb
@@ -10,7 +10,15 @@ class Score
     10 => ['Q', 'Z']
   }
 
+  def self.not_a_word?(input_string)
+    # if string contains punctuation or a number, it will not be nil
+    return false if /[\W[0-9]]/.match(input_string).nil?
+    true
+  end
+
   def self.score(word)
+    # check if word is actually a word (not containing punctuation or numbers)
+    return "that's not a word!" if self.not_a_word?(word)
     word_score = 0
     word = word.upcase
     letter_array = word.split(//)
@@ -29,7 +37,14 @@ class Score
   end
 
   def self.score_many(list_of_words)
-    list_of_words.gsub!(",", "")
+    # remove any commas that may be separating words
+    # replace with a space in case there's no space after the commas
+    list_of_words.gsub!(",", " ")
+
+    # call method not_a_word? to determine whether words contain any
+    # non-letter characters like punctuation or numbers
+    return "those aren't all words!" unless self.not_a_word?(list_of_words)
+    
     array_of_words = list_of_words.split(" ")
     word_hash = {}
 
